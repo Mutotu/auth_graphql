ef2ee125aa0f404cf654fa599d6d7471b79a3e8c
mutotu <muttalip.aydemir@gmail.com>
2022-11-01 15:34:53 -0700

    added dashboard and changes for it in the forms

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,11 +3,20 @@ import AuthForm from "./AuthForm";
 import mutation from "../mutations/Login";
 import { graphql } from "react-apollo";
 import query from "../queries/CurrentUser";
+import { hashHistory } from "react-router";
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = { errors: [] };
+  }
+  componentWillUpdate(nextProps) {
+    // this.props; // the old, current set of props
+    // nextProps; // the next set of props that will be in place when the component rerenders
+    if (!this.props.data.user && nextProps.data.user) {
+      //redirect to dashborad
+      hashHistory.push("/dashboard");
+    }
   }
   onSubmit({ email, password }) {
     this.props
@@ -34,4 +43,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(graphql(mutation)(LoginForm));
