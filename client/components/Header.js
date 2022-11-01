f60bd56b1a82a5658a0150730959fed964ab437f
mutotu <muttalip.aydemir@gmail.com>
2022-11-01 07:57:00 -0700

    adding log out functionality

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import query from "../queries/CurrentUser";
 import { Link } from "react-router";
+import mutation from "../mutations/Logout";
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }],
+    });
+  }
   renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) return <div />;
-    if (user) return <div>Logout</div>;
+    if (user)
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Log out</a>
+        </li>
+      );
     else
       return (
         <div>
@@ -35,4 +46,4 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
